7ea449ac373a5d1df5329e9bc4ae8b070803e6d0
MalteHerrmann <malteherrmann.mail@web.de>
2022-08-02 12:19:25 +0200

    Create logparser package

--- main.go
+++ main.go
@@ -3,46 +3,33 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"github.com/ethereum/go-ethereum"
-	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	logparser "github.com/MalteHerrmann/TrackGovernance/parser"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 )
 
-// SubscribeToBlocks subscribes to new blocks and prints them to the console.
-func SubscribeToBlocks(client *ethclient.Client) {
-	// Create a subscription that fires on new blocks.
-	ctx := context.Background()
-
-	logs := make(chan ethtypes.Log)
-	sub, err := client.SubscribeFilterLogs(ctx, ethereum.FilterQuery{}, logs)
-	if err != nil {
-		log.Fatalf("Failed to subscribe to logs: %v", err)
-	}
-	fmt.Println("Subscribed to logs")
-
-	for {
-		select {
-		case err := <-sub.Err():
-			log.Fatalf("Failed to subscribe to logs: %v", err)
-		case block := <-logs:
-			fmt.Println("Block: ", block.BlockHash.Hex())
-		}
-	}
-}
-
 func main() {
+	// Print info
+	fmt.Println("")
+	fmt.Println("----------------------------------------------------")
+	fmt.Println("                 TrackGovernance")
+	fmt.Println("----------------------------------------------------")
 	// Create a new instance of the API client from the client package.
 	// The client is used to communicate with the node.
 	//
-	//client, err := ethclient.Dial("wss://eth.bd.evmos.org:8546")
-	client, err := ethclient.Dial("wss://mainnet.infura.io/ws/v3/266df44dd6b24b39ba5bb703049aa3c8")
+	url := "wss://mainnet.infura.io/ws/v3/266df44dd6b24b39ba5bb703049aa3c8"
+	//url := "wss://eth.bd.evmos.org:8546"
+	client, err := ethclient.Dial(url)
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
+	fmt.Println("Connected to Ethereum client at " + url)
+	fmt.Println("")
+
+	// Create log parser
+	lp := logparser.NewLogParser(client)
 
 	// Subscribe to new blocks
-	SubscribeToBlocks(client)
+	lp.SubscribeToBlocks()
 }
---
+++ parser/log_parser.go
@@ -0,0 +1,105 @@
+package logparser
+
+import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+)
+
+// LogParser is a type that can be used to parse logs.
+type LogParser struct {
+	// The client used to communicate with the node.
+	client *ethclient.Client
+
+	// The channel used to receive new blocks.
+	logs chan ethtypes.Log
+
+	// The subscription used to receive new blocks.
+	sub ethereum.Subscription
+
+	// Last seen block number
+	lastBlockNumber uint64
+
+	// Last seen transaction info
+	lastTxIndex uint
+	lastTxHash  common.Hash
+
+	// Failed transactions
+	FailedTxs []common.Hash
+}
+
+// NewLogParser creates a new LogParser.
+func NewLogParser(client *ethclient.Client) *LogParser {
+	return &LogParser{
+		client: client,
+		logs:   make(chan ethtypes.Log),
+	}
+}
+
+// SubscribeToBlocks subscribes to new blocks and processes the
+// received information.
+func (lp *LogParser) SubscribeToBlocks() {
+	// Subscribe with empty filter settings to receive all logs
+	sub, err := lp.client.SubscribeFilterLogs(context.Background(), ethereum.FilterQuery{}, lp.logs)
+	if err != nil {
+		log.Fatalf("Failed to subscribe to logs: %v", err)
+	}
+	lp.sub = sub
+
+	//fmt.Print("\033[s")
+	for {
+		select {
+		case err := <-lp.sub.Err():
+			log.Fatalf("Failed to subscribe to logs: %v", err)
+		case block := <-lp.logs:
+			lp.processLog(block)
+		}
+	}
+}
+
+// ProcessLog is responsible for the further processing of a received
+// log.
+func (lp *LogParser) processLog(block ethtypes.Log) {
+	currentBlocknumber := block.BlockNumber
+	currentTxIndex := block.TxIndex
+	fmt.Printf("\rBlock %d - tx %d", currentBlocknumber, currentTxIndex)
+
+	if currentBlocknumber > lp.lastBlockNumber {
+		lp.lastBlockNumber = currentBlocknumber
+	}
+	if currentTxIndex > lp.lastTxIndex {
+		// always processes the last tx when a new tx comes in
+		ok := lp.ProcessTx(lp.lastTxHash)
+		if !ok {
+			lp.FailedTxs = append(lp.FailedTxs, lp.lastTxHash)
+			fmt.Printf(" Failed transactions: %v", len(lp.FailedTxs))
+		}
+
+		// Assign new tx info
+		lp.lastTxIndex = block.TxIndex
+		lp.lastTxHash = block.TxHash
+	}
+}
+
+// ProcessTx queries the transaction with the given
+// transaction hash and processes the received information.
+func (lp *LogParser) ProcessTx(txHash common.Hash) bool {
+	_, isPending, err := lp.client.TransactionByHash(context.Background(), txHash)
+	if err != nil {
+		fmt.Printf(" Failed to get transaction: %v", txHash.Hex())
+		return false
+		//log.Fatalf("\rFailed to get transaction: %v\n%v", txHash.Hex(), err)
+	}
+
+	if isPending {
+		fmt.Printf(" Transaction %v is pending", txHash.Hex())
+	} else {
+		fmt.Printf(" Transaction %v is not pending", txHash.Hex())
+	}
+
+	return true
+}
